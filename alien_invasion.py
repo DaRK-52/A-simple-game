63b3556b5a0db85ed1e320c3e9ad0aff3949bceb
DaRK52 <zhuwenjun@sjtu.edu.cn>
2021-01-23 15:24:04 +0800

    Bring the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     #构造函数
@@ -11,6 +12,7 @@ class AlienInvasion:
         self.settings=Settings()
         self.screen=pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship=Ship(self)
     #游戏运行主函数
     def run_game(self):
         while True:
@@ -18,6 +20,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitime()
             pygame.display.flip()
 
 if __name__=='__main__':
